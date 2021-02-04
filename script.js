796bfe9a9257118426ec72cbaaa6f3e10cdd7a2c
jfadgyas <74911281+jfadgyas@users.noreply.github.com>
2021-02-04 12:57:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const pizzaMaking = () => {
+    console.log('make dough');
+    console.log('put tomato+salami');
+    console.log('bake pizza');
+}
+
+const sushiMaking = () => {
+    console.log('go fishing');
+    console.log('cook rice');
+    console.log('roll sushi');
+}
+
+sushiMaking();
+sushiMaking();
+sushiMaking();
+
+pizzaMaking();
+pizzaMaking();
